a4339155a9db90faecb55a0d1082dc4803fbefbd
Aldo Mendez <aldomendez@berkeley.edu>
2016-12-06 02:00:36 -0800

    made some progress on weather analyzer method

--- weatherGuru/weatherGuru/ViewController.swift
+++ weatherGuru/weatherGuru/ViewController.swift
@@ -18,6 +18,57 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     //TODO:for now I am only getting the weather not yet activites
     //Aldo, and I will figure that part.
     //Very hard coded
+    
+    //
+    
+   var categorizedOutput: [String: String] = [
+        "weather": "not known",
+        "temp": "0",
+        "wind": "0.0"
+    ]
+    
+    var weather: String = ""
+    var temp: Int = 0
+    var windSpeed: Double = 0.0
+    
+    var weatherSummary: String = ""
+    
+    //I created this method to extract the crucial information from the forecast in order to turn it into the prediction for the activity. Still needs a little bit of work.
+    func analyzeWeather() {
+        //Analyzes weather, temperature and wind
+        //Reference: http://stackoverflow.com/questions/27880650/swift-extract-regex-matches
+        
+        //Set weather variable for recommendation
+        let weather = categorizedOutput["weather"]!
+        let temp = categorizedOutput["temp"]!
+        let wind = categorizedOutput["wind"]!
+        if (weather.contains("rain") || weather.contains("Rain")) {
+            self.weather = "rain"
+        }
+        
+        //Set temperature variable for recommendation
+        //let regex = try! NSRegularExpression(pattern: "/([\s\S]\d\d)/")
+        let regex = try! NSRegularExpression(pattern: "[0-9][0-9]|[0-9]");
+        let nsString = temp as NSString
+        let results = regex.matches(in: temp, range: NSRange(location: 0, length: nsString.length))
+        let matches = results.map { nsString.substring(with: $0.range)}
+        self.temp = Int(matches[0])!
+        //print(matches)
+        //var matches = regex.matches(in: temp, range: NSRange(location: 0, length: temp.characters.count))
+        //print(temp)
+        
+        
+        //Set windSpeed variable for recommendation, only really used to make sure not to recommend anything during a hurricane or tornado
+        //Note: give lower priority
+        //let nsString_wind = temp as NSString
+        //let results_wind = regex.matches(in: wind, range: NSRange(location: 0, length: nsString_wind.length))
+        //let matches_wind = results_wind.map { nsString_wind.substring(with: $0.range)}
+        //self.windSpeed = Double(matches_wind[0])!
+        
+        print(self.temp)
+        //print(self.windSpeed)
+    }
+    
     @IBAction func findWeather(_ sender: AnyObject) {
         var wasSuccessful = false
         //Go to this url and fetch weather for the city entered.
@@ -33,11 +84,19 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
                         let weatherArray = websiteArray![1].components(separatedBy: "</span>")
                         if weatherArray.count > 1 {
                             wasSuccessful = true
-                            let weatherSummary = weatherArray[0].replacingOccurrences(of: "&deg;", with: "º")
+                            self.weatherSummary = weatherArray[0].replacingOccurrences(of: "&deg;", with: "º")
                             DispatchQueue.main.async(execute: { () -> Void in
-                                self.resultsLabel.text = weatherSummary
+                                self.resultsLabel.text = self.weatherSummary
                             })
                         }
+                        
+                        //Yadel: I added a few things here as well as made weatherSummary a global variable.
+                        //The basic idea is the following: resultsLabel is essentially a paragraph with 3 sentences. The first sentence contains information on precipitation (i.e., raining, drizzling, sunny, cloudy, etc.). The second sentence has temperature, and the third is wind speed. We probably won't really need the last one so let's prioritize on the other 2. I have thus split weatherSummary into 3 sentences that we will then analyze in the method "analyzeWeather"
+                        let splitWeatherArray = self.weatherSummary.components(separatedBy: ".")
+                        self.categorizedOutput["weather"] = splitWeatherArray[0]
+                        self.categorizedOutput["temp"] = splitWeatherArray[1]
+                        self.categorizedOutput["wind"] = splitWeatherArray[2]
+                        self.analyzeWeather()
                     }
                     
                     if wasSuccessful == false {
